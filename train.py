e7481f60ef4cbf8a1b48737fba293bd102c6a44c
mjq-520 <mjq520521@163.com>
2025-10-01 22:33:03 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,224 @@
+from __future__ import division
+
+import argparse
+import copy
+import logging
+import mmcv
+import os
+import time
+import torch
+import warnings
+from mmcv import Config, DictAction
+# from mmengine.config import Config
+# from mmcv.runner import get_dist_info, init_dist
+from mmengine.dist.utils import get_dist_info, init_dist
+from os import path as osp
+
+from mmdet3d import __version__
+from mmdet3d.datasets import build_dataset
+from mmdet3d.models import build_detector
+from mmdet3d.utils import collect_env, get_root_logger
+from mmdet.apis import set_random_seed
+
+from plugin.track.api.train import train_tracker
+
+# small helper to print trainable params
+from prettytable import PrettyTable
+
+
+def count_parameters(model, logger):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        param = parameter.numel()
+        table.add_row([name, param])
+        total_params += param
+    logger.info(table)
+    logger.info(f"Total Trainable Params: {total_params}")
+    return total_params
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a detector')
+    parser.add_argument('config', help='train config file path')
+    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser.add_argument(
+        '--resume-from', help='the checkpoint file to resume from')
+    parser.add_argument(
+        '--no-validate',
+        action='store_true',
+        help='whether not to evaluate the checkpoint during training')
+    group_gpus = parser.add_mutually_exclusive_group()
+    group_gpus.add_argument(
+        '--gpus',
+        type=int,
+        help='number of gpus to use '
+        '(only applicable to non-distributed training)')
+    group_gpus.add_argument(
+        '--gpu-ids',
+        type=int,
+        nargs='+',
+        help='ids of gpus to use '
+        '(only applicable to non-distributed training)')
+    parser.add_argument('--seed', type=int, default=0, help='random seed')
+    parser.add_argument(
+        '--deterministic',
+        action='store_true',
+        help='whether to set deterministic options for CUDNN backend.')
+    parser.add_argument(
+        '--options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file (deprecate), '
+        'change to --cfg-options instead.')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument(
+        '--autoscale-lr',
+        action='store_true',
+        help='automatically scale lr with the number of gpus')
+    args = parser.parse_args()
+    if 'LOCAL_RANK' not in os.environ:
+        os.environ['LOCAL_RANK'] = str(args.local_rank)
+
+    if args.options and args.cfg_options:
+        raise ValueError(
+            '--options and --cfg-options cannot be both specified, '
+            '--options is deprecated in favor of --cfg-options')
+    if args.options:
+        warnings.warn('--options is deprecated in favor of --cfg-options')
+        args.cfg_options = args.options
+
+    return args
+
+
+def main():
+    # torch.autograd.set_detect_anomaly(True)
+    args = parse_args()
+
+    cfg = Config.fromfile(args.config)
+    
+    if hasattr(cfg, 'plugin') & cfg.plugin:
+        import importlib
+        if hasattr(cfg, 'plugin_dir'):
+            plugin_dir = cfg.plugin_dir
+            _module_dir = os.path.dirname(plugin_dir)
+            _module_dir = _module_dir.split('/')
+            _module_path = _module_dir[0]
+
+            for m in _module_dir[1:]:
+                _module_path = _module_path + '.' + m
+            print(_module_path)
+            plg_lib = importlib.import_module(_module_path)
+        else:
+            # import dir is the dirpath for the config file
+            _module_dir = os.path.dirname(args.config)
+            _module_dir = _module_dir.split('/')
+            _module_path = _module_dir[0]
+            for m in _module_dir[1:]:
+                _module_path = _module_path + '.' + m
+            print(_module_path)
+            plg_lib = importlib.import_module(_module_path)
+
+    # work_dir is determined in this priority: CLI > segment in file > filename
+    if args.work_dir is not None:
+        # update configs according to CLI args if args.work_dir is not None
+        cfg.work_dir = args.work_dir
+    elif cfg.get('work_dir', None) is None:
+        # use config filename as default work_dir if cfg.work_dir is None
+        cfg.work_dir = osp.join('./work_dirs',
+                                osp.splitext(osp.basename(args.config))[0])
+    
+    cfg.gpu_ids = range(1) if args.gpus is None else range(args.gpus)
+
+   # init distributed env first, since logger depends on the dist info.
+    
+    distributed = False
+
+    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+    # dump config
+    cfg.dump(osp.join(cfg.work_dir, osp.basename(args.config)))
+    # init the logger before other steps
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
+    logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
+
+    # add a logging filter
+    logging_filter = logging.Filter('mmdet')
+    logging_filter.filter = lambda record: record.find('mmdet') != -1
+
+    # init the meta dict to record some important information such as
+    # environment info and seed, which will be logged
+    meta = dict()
+    # log env info
+    env_info_dict = collect_env()
+    env_info = '\n'.join([(f'{k}: {v}') for k, v in env_info_dict.items()])
+    dash_line = '-' * 60 + '\n'
+    logger.info('Environment info:\n' + dash_line + env_info + '\n' +
+                dash_line)
+    meta['env_info'] = env_info
+    meta['config'] = cfg.pretty_text
+
+    # log some basic info
+    logger.info(f'Distributed training: {distributed}')
+    logger.info(f'Config:\n{cfg.pretty_text}')
+
+    # set random seeds
+    if args.seed is not None:
+        logger.info(f'Set random seed to {args.seed}, '
+                    f'deterministic: {args.deterministic}')
+        set_random_seed(args.seed, deterministic=args.deterministic)
+    cfg.seed = args.seed
+    meta['seed'] = args.seed
+    meta['exp_name'] = osp.basename(args.config)
+
+    model = build_detector(
+        cfg.model,
+        train_cfg=cfg.get('train_cfg'),
+        test_cfg=cfg.get('test_cfg'))
+
+    logger.info(f'Model:\n{model}')
+    datasets = [build_dataset(cfg.data.train)]
+
+    if cfg.checkpoint_config is not None:
+        # save mmdet version, config file content and class names in
+        # checkpoints as meta data
+        cfg.checkpoint_config.meta = dict(
+            mmdet_version=__version__,
+            config=cfg.pretty_text,
+            CLASSES=datasets[0].CLASSES)
+    # add an attribute for visualization convenience
+    model.CLASSES = datasets[0].CLASSES
+
+    print("Count trainable params")
+    count_parameters(model, logger)
+
+    train_tracker(
+        model,
+        datasets,
+        cfg,
+        distributed=distributed,
+        validate=(not args.no_validate),
+        timestamp=timestamp,
+        meta=meta)
+
+
+if __name__ == '__main__':
+    main()
